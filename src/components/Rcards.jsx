b5eebb1cc7dc9acd26e9975959f884b96633c96e
Wainaina Erastus <wainainaerastus2@gmail.com>
2026-01-29 17:05:40 +0300

    working with map methods and a key

--- src/components/Rcards.jsx
+++ src/components/Rcards.jsx
@@ -1,23 +1,20 @@
-import React from 'react'
-
 function Rcards() {
+  const cardData = [
+    { id: 1, title: "HTML", desc: "The structure" },
+    { id: 2, title: "CSS", desc: "The style" },
+    { id: 3, title: "JS", desc: "The logic" }
+  ];
+
   return (
-   <main className=' flex flex-col h-screen items-center justify-center bg-green-100 gap-6 '>
-    <img src="react.svg" className='absolute bottom-4 right-4 w-10 opacity-20 group-hover:opacity-100 transition-opacity' />
-    <h1 className='text-3xl font-bold mb-6  '>Fun Facts About React</h1>
-    <div className='flex'>
-        <ul className='max-w-90 bg-green-600 transition-transform hover:translate-x-3 hover:-translate-y-4 p-6 rounded-lg text-white hover:shadow-[0_20px_50px_rgba(8,_112,_184,_0.7)] divide-y divide-green-100 '>
-            <li  className='p-4 font-bold'>1. Was first released in 2013</li>
-            <li  className='p-4 font-bold'> 2.Was originally created by Jordan Walke</li>
-            <li  className='p-4 font-bold'> 3.Has well over 100k stars on GitHub</li>
-            <li  className='p-4 font-bold'>4. Is maintained by Meta</li>
-            <li className='p-4 font-bold'>5. Powers thousands of enterprise apps</li>
-        </ul>
+    <div className="flex gap-4 p-4">
+      {cardData.map((card) => (
+        <div key={card.id} className="bg-zinc-800 p-6 rounded-lg shadow-lg">
+          <h3 className="text-xl font-bold text-white">{card.title}</h3>
+          <p className="text-gray-400">{card.desc}</p>
+        </div>
+      ))}
     </div>
-    
-   
-</main>
-  )
+  );
 }
 
 export default Rcards
