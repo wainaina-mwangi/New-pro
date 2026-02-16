310906b43d69c03a641f003b4181108f9799e111
Wainaina Erastus <wainainaerastus2@gmail.com>
2026-02-16 15:57:35 +0300

    learnt how i can make a faqs method in react using useState

--- src/components/Joke.jsx
+++ src/components/Joke.jsx
@@ -1,18 +1,18 @@
 import React, { useState } from 'react'
 
-function Joke({ setup, punchline }) {
+function Joke(props) {
   const [isShown,setIsShown] = useState(false)
   function toggleShown(){
     setIsShown(prevShown=>!prevShown)
-    console.log('isShown')
+    
   }
   return (
     <div className=" bg-zinc-900 border border-zinc-800 p-6 rounded-xl my-4 shadow-lg max-w-md">
       <p className="text-zinc-400 text-sm font-mono mb-2 uppercase tracking-widest">The Setup:</p>
-      <h3 className="text-xl font-semibold text-white mb-4">{setup}</h3>
+      <h3 className="text-xl font-semibold text-white mb-4">{props.setup}</h3>
       
       <p className="text-zinc-400 text-sm font-mono mb-2 uppercase tracking-widest">The Punchline:</p>
-      <p className="text-lg text-blue-400 italic font-medium">"{punchline}"</p>
+    { isShown && <p className="text-lg text-blue-400 italic font-medium">"{props.punchline}"</p>}
       <button onClick={toggleShown}>Show</button>
     </div>
   )
