77ad8374fdebaf0cf857401b75a991137b1df691
Wainaina Erastus <wainainaerastus2@gmail.com>
2026-02-16 15:36:49 +0300

    added a feature for show toggle in my learning react app

--- src/components/Joke.jsx
+++ src/components/Joke.jsx
@@ -1,6 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Joke({ setup, punchline }) {
+  const [isShown,setIsShown] = useState(false)
+  function toggleShown(){
+    setIsShown(prevShown=>!prevShown)
+    console.log('isShown')
+  }
   return (
     <div className=" bg-zinc-900 border border-zinc-800 p-6 rounded-xl my-4 shadow-lg max-w-md">
       <p className="text-zinc-400 text-sm font-mono mb-2 uppercase tracking-widest">The Setup:</p>
@@ -8,6 +13,7 @@ function Joke({ setup, punchline }) {
       
       <p className="text-zinc-400 text-sm font-mono mb-2 uppercase tracking-widest">The Punchline:</p>
       <p className="text-lg text-blue-400 italic font-medium">"{punchline}"</p>
+      <button onClick={toggleShown}>Show</button>
     </div>
   )
 }
